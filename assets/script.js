2dedee1ab2d9dfa19e0b071cc88172dff1d0fcc8
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-09-05 18:33:52 +1000

    Add background image + classes in CSS

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// Start button function 
+
+// Timer countdown 
+    
+
+// Conditional statements for the questions
+    // Note: Window alert prompts for Week 3 Challenge
+
+    // If incorrect answer, reduce the time - Refer to password generator 
+    // Add variables 'correct' and 'incorrect' and assign values
+    // If answer correct + 5
+    // If incorrect - 5 
+
+
