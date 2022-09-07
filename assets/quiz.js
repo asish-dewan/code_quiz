0b1d1ddd7a3b4672be5bd76f62b531166648b812
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-09-08 01:28:35 +1000

    Added variables and question for quiz

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,14 +1,69 @@
-// Start button function 
+//Define variables
 
-// Timer countdown 
-    
+const question = document.querySelector('#question');
+const choices = Array.from (document.querySelector('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
-// Conditional statements for the questions
-    // Note: Window alert prompts for Week 3 Challenge
+let currentQuestion = '';
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
-    // If incorrect answer, reduce the time - Refer to password generator 
-    // Add variables 'correct' and 'incorrect' and assign values
-    // If answer correct + 5
-    // If incorrect - 5 
+let questions = [
+    {
+        question: 'The "function" and "var" are known as: '
+        choice1: 'Keywords',
+        choice2: 'Data types',
+        choice3: 'Declaration statements',
+        choice4: 'Prototypes',
+        answer: 3,
+    },
+    {
+        question: 'Which one of the following is the correct way for calling the JavaScript code? '
+        choice1: 'Preprocessor',
+        choice2: 'Triggering Event',
+        choice3: 'RMI',
+        choice4: 'Function/Method',
+        answer: 4,
+    },
+    {
+        question: ' Which of the following type of a variable is volatile? '
+        choice1: 'Mutable variable',
+        choice2: 'Dynamic variable',
+        choice3: 'Volatile variable',
+        choice4: 'Immutable variable',
+        answer: 1,
+    },
+    {
+        question: 'Which of the following number object function returns the value of the number?'
+        choice1: 'toString()',
+        choice2: 'valueOf()',
+        choice3: 'toLocaleString()',
+        choice4: 'toPrecision()',
+        answer: 2,
+    }
+]
+
+const score_points = 100;
+const max_questions = 4;
+
+startQuiz = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion ()
+}
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > max_questions) {
+        localStorage.setItem('mostRecentScore',score)
+
+        return window.location.assign("/end.html")
+    }
+
+}
 
 
